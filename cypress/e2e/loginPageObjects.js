7f373a0ffb86e2d01c5662ba380d6c2d7d550bc4
Lucas <nerffwurf@gmail.com>
2024-02-29 21:02:11 +0000

    -chore add page data

---
+++ cypress/e2e/loginPageObjects.js
@@ -0,0 +1,6 @@
+export const logoTitle = "login_logo"
+export const loginUsernameField = "username"
+export const loginPasswordField = "password"
+export const loginButton = "login-button"
+export const errorAlert = "error"
+export const errorButton = "error-button"
