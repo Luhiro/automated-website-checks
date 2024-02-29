2a027b0f2bb40baafa51e15f2338a80f6eb67246
Lucas <nerffwurf@gmail.com>
2024-02-29 21:02:38 +0000

    -chore add static data

---
+++ cypress/e2e/constants.js
@@ -0,0 +1,4 @@
+export const lockedOutErrorMessage = "Epic sadface: Sorry, this user has been locked out.";
+export const AccessErrorMessage = "Epic sadface: You can only access '/inventory.html' when you are logged in.";
+export const password = "secret_sauce"
+export const defectImage = "/static/media/sl-404.168b1cce.jpg"
